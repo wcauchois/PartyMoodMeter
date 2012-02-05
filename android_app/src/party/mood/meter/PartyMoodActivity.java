e763f5304bd1aea678c5afc02d32955acf6c6c39
Bill Cauchois <wcauchois@gmail.com>
2012-02-04 17:11:40 -0800

    add "stop partying" button to PartyMoodActivity/layout

--- android_app/src/party/mood/meter/PartyMoodActivity.java
+++ android_app/src/party/mood/meter/PartyMoodActivity.java
@@ -7,5 +7,6 @@ public class PartyMoodActivity extends Activity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    setContentView(R.layout.party_mood);
   }
 }
