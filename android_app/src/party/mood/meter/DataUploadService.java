07dd9c0a8434b045295a95d255308918a3c32c7c
Bill Cauchois <wcauchois@gmail.com>
2012-02-17 12:17:34 -0800

    use measure of "time averaged average delta" in datauploadservice

--- android_app/src/party/mood/meter/DataUploadService.java
+++ android_app/src/party/mood/meter/DataUploadService.java
@@ -2,6 +2,7 @@ package party.mood.meter;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Arrays;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -11,6 +12,7 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 import android.app.Service;
 import android.content.Context;
@@ -23,7 +25,7 @@ import android.os.IBinder;
 import android.widget.Toast;
 
 public class DataUploadService extends Service {
-  private final int UPLOAD_PERIOD = 5;
+  private static final int UPLOAD_PERIOD = 5;
   private static final String API_ENDPOINT = Utils.SITE_ROOT + "/submit_sensor";
   
   private ScheduledExecutorService mScheduler;
@@ -34,22 +36,28 @@ public class DataUploadService extends Service {
   private static class UploadSensorData implements Runnable, SensorEventListener {
     private URI mApiEndpoint;
     private float[] mCurrentValues = null;
+    private float[] mValueDeltas;
     
     public UploadSensorData(URI apiEndpoint) {
       mApiEndpoint = apiEndpoint;
     }
     
     public void run() {
-      if(mCurrentValues == null)
+      if(mValueDeltas == null)
         return;
-      JSONArray arrayToSubmit = new JSONArray();
       try {
         // TODO: we should attach some kind of uid to this post
-        for(float val : mCurrentValues)
-          arrayToSubmit.put(val);
+        float timeAveragedAverageDelta = 0.0f;
+        for(int i = 0; i < mValueDeltas.length; i++) {
+          timeAveragedAverageDelta +=
+              (mValueDeltas[i] / (UPLOAD_PERIOD * 1000.0f * (float)mValueDeltas.length));
+        }
+        mValueDeltas = null;
+        JSONObject objectToSubmit = new JSONObject();
+        objectToSubmit.put("taad", timeAveragedAverageDelta);
         
         HttpPost post = new HttpPost(mApiEndpoint);
-        post.setEntity(new StringEntity(arrayToSubmit.toString()));
+        post.setEntity(new StringEntity(objectToSubmit.toString()));
         HttpClient client = new DefaultHttpClient();
         client.execute(post);
       } catch(Exception e) {
@@ -60,6 +68,14 @@ public class DataUploadService extends Service {
     public void onAccuracyChanged(Sensor sensor, int accuracy) { }
 
     public void onSensorChanged(SensorEvent evt) {
+      if(mCurrentValues != null) {
+        if(mValueDeltas == null) {
+          mValueDeltas = new float[evt.values.length];
+          Arrays.fill(mValueDeltas, 0.0f);
+        }
+        for(int i = 0; i < evt.values.length; i++)
+          mValueDeltas[i] += Math.abs(evt.values[i] - mCurrentValues[i]); 
+      }
       mCurrentValues = new float[evt.values.length];
       System.arraycopy(evt.values, 0, mCurrentValues, 0, evt.values.length);
     }
